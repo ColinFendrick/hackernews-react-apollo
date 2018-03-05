baf255c4df39b380cb7a104965ed2657f5a2870b
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-05 09:26:01 -0500

    Subscriptions added over websocket

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -12,6 +12,81 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
+  _subscribeToNewLinks = () => {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+      subscription {
+        newLink {
+          node {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+              id
+              name
+            }
+            votes {
+              id
+              user {
+                id
+              }
+            }
+          }
+        }
+      }
+    `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [subscriptionData.data.newLink.node, ...previous.feed.links]
+        const result = {
+          ...previous,
+          feed: {
+            links: newAllLinks
+          }
+        }
+        return result
+      }
+    })
+  }
+
+  _subscribeToNewVotes = () => {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+      subscription {
+        newVote {
+          node {
+            id
+            link {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+            user {
+              id
+            }
+          }
+        }
+      }
+    `,
+    })
+  }
+
+  componentDidMount() {
+    this._subscribeToNewLinks()
+    this._subscribeToNewVotes()
+  }
+
   render() {
     const { feedQuery } = this.props
     if (feedQuery && feedQuery.loading) {
